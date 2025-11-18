fbeb8cbed0cc6403f10d070634989525141774e0
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-18 21:25:48 +0900

    feat: base64 인코딩/디코딩 구현

--- server/chat/views.py
+++ server/chat/views.py
@@ -1,3 +1,4 @@
+import base64
 from secrets import token_bytes
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from os import environ
@@ -34,9 +35,21 @@ def create_chat_room():
     """
     pass
 
-# AES-GCM 암호화 함수
-def encrypt_aes_gcm(master_key: bytes, plaintext: bytes, iv: bytes):
+# AES-GCM 암호화 및 Base64 인코딩
+def encrypt_aes_gcm(master_key: bytes, plaintext: bytes, iv: bytes) -> str:
     aesgcm = AESGCM(master_key)
     ciphertext = aesgcm.encrypt(iv, plaintext, None)
 
-    return ciphertext
+    encrypted_blob = iv + ciphertext
+    return base64.b64encode(encrypted_blob).decode()
+
+# Base64 디코딩 및 AES-GCM 복호화
+def decrypt_aes_gcm(master_key: bytes, b64_data: str) -> bytes:
+    aesgcm = AESGCM(master_key)
+    data = base64.b64decode(b64_data)
+
+    iv = data[:12]
+    ciphertext = data[12:]
+
+    secret_key = aesgcm.decrypt(iv, ciphertext, None)
+    return secret_key
