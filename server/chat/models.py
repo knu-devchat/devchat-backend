2dad64901a63a166cdfe9f39dbf0a15db049a726
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-18 21:21:30 +0900

    feat: chat 앱 model 구현

--- server/chat/models.py
+++ server/chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SecureData(models.Model):
+    chat_id = models.IntegerField()
+    chat_name = models.CharField(max_length=20)
+    encrypted_value = models.TextField() # base64 암호문 저장
+    created_at = models.DateTimeField(auto_now_add=True)
