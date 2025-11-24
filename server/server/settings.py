870ac1d25b935873839925961ba95c46f1a8b939
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-24 15:42:40 +0900

    chore: host allow 설정

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,9 +39,12 @@ if len(MASTER_KEY) not in (16, 24, 32):
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DEBUG', 'False').lower == 'true'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+CORS_ALLOWED_ALL_ORIGINS = os.environ.get('CORS_ALLOWED_ALL_ORIGINS', 'False').lower() == 'true'
+if not CORS_ALLOWED_ALL_ORIGINS:
+    CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 # Application definition
