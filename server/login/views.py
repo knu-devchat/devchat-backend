572248f7475880111cc1f0d4aaebc2155a927f8b
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-24 17:32:49 +0900

    test

--- server/login/views.py
+++ server/login/views.py
@@ -1,15 +1,16 @@
 from django.views.decorators.http import require_GET
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
-from django.middleware.csrf import get_token
 from django.urls import reverse
 from urllib.parse import urlencode
 
 
 
 # Create your views here.
+@ensure_csrf_cookie
 def login(request):
     if request.user.is_authenticated:
         return redirect(settings.LOGIN_REDIRECT_URL)
@@ -32,24 +33,9 @@ def github_auth_url(request):
     params = {'process': 'login'}
     if next_url:
         params['next'] = next_url
-        qs = urlencode(params)
-        full = request.build_absolute_uri(login_path) + ('?' + qs if qs else '')
-
-        # Return an auto-submitting POST form so the allauth provider flow
-        # starts immediately (allauth expects a POST to begin provider auth).
-        csrf_token = get_token(request)
-        html = f'''<!doctype html>
-<html>
-    <head><meta charset="utf-8"></head>
-    <body>
-        <form id="authForm" method="post" action="{full}">
-            <input type="hidden" name="csrfmiddlewaretoken" value="{csrf_token}">
-        </form>
-        <script>document.getElementById('authForm').submit();</script>
-    </body>
-</html>'''
-        return HttpResponse(html)
-
+    qs = urlencode(params)
+    full = request.build_absolute_uri(login_path) + ('?' + qs if qs else '')
+    return JsonResponse({'login_url': full})
 
 def logout_url(request):
     try:
